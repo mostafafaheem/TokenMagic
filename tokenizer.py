2e595c69423c4ec0285ee5548041f0dc517dc5b5
Mostafa <mostafas.main.email@gmail.com>
2025-09-10 18:43:59 +0300

    finalized tokenzer training method

--- tokenizer.py
+++ tokenizer.py
@@ -17,7 +17,7 @@ class BPETokenizer:
     def __init__(self, pattern = None):
         self.pattern = re.compile(pat_str) if pattern is None else pattern
 
-    def train(self, vocabulary_size: int, text, verbose: bool=False):
+    def train(self, vocabulary_size: int, text: str, verbose: bool=False):
         
         if vocabulary_size < 2**8:
             raise ValueError("Vocab size must be at least 256 in order to encode all possible characters.")
@@ -29,14 +29,17 @@ class BPETokenizer:
         word_tokens: list[list[bytes]] = [
             [bytes([byte]) for byte in word.encode("utf-8")] for word in self.pattern.findall(text)
         ]
+
+        # print(word_tokens)
         merges = {}
         while num_merges > 0:
             pair_counts = collections.Counter()
             for word in word_tokens:
                 for byte_pair in zip(word[:-1], word[1:]):
-                    pair_counts[byte_pair] + 1
-            
-            most_frequent_pair = max(pair_counts, key=lambda x: pair_counts[x])
+                    pair_counts[byte_pair] += 1
+            if not pair_counts:
+                break
+            most_frequent_pair = max(pair_counts, key=pair_counts.get)
             merged_bytes = most_frequent_pair[0] + most_frequent_pair[1]
             merges[most_frequent_pair] = vocabulary_size - num_merges
             ids[vocabulary_size - num_merges] = merged_bytes
@@ -52,18 +55,17 @@ class BPETokenizer:
                     else:
                         merged_word.append(word[i])
                         i += 1
+                if i == len(word) - 1:
+                    merged_word.append(word[i])
+                merged_word_tokens.append(merged_word)
+            
+            word_tokens = merged_word_tokens
+
             num_merges -= 1
-        pass
+        print(merges)
+        return ids
 
     def encode(self, text: str) -> list[int]:
         pass
     def decode(self, tokens_ids: list[int]) -> str:
         pass
-    
-        # vocab = {bytes([i]):i for i in range(256)}
-
-        # return vocab
-# for i, x in bpe_train().items():
-#     print(f"{i}, {x}")
-# print(bpe_train())
-
